457e841626eaa6877e440990a770a36fe61c3b21
brendenozie <brendenozie@gmail.com>
2024-04-26 22:29:47 +0300

    home update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -98,8 +98,8 @@ const Home = ({ citiesData, stylesData, getInspiredCities }: Props) => {
 
       <main className="max-w-full mx-auto md:w-9/12">
         <section className="py-6 my-28 dark:text-gray-50">
-            <div className="container grid grid-cols-2 gap-4 mx-auto md:grid-cols-4 justify-evenly">              
-              <div className="block w-full h-3/4 col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 aspect-auto p-2">
+            <div className="container grid grid-cols-2 gap-4 mx-auto md:grid-cols-4 justify-evenly items-center text-center">              
+              <div className="block w-full h-3/4 col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 aspect-auto p-2 mb-10 md:mb-0">
                   <div className="h-full flex flex-col justify-between">
                     <div>
                       <h3 className="text-5xl font-bold text-gray-700">
@@ -218,7 +218,7 @@ const Home = ({ citiesData, stylesData, getInspiredCities }: Props) => {
         <section>
           <div className="mx-auto max-w-screen-xl py-8 sm:py-12">
             <header>
-              <h2 className="text-5xl font-bold text-gray-900 sm:text-3xl w-full items-center text-center m-8">Best Place Destination</h2>
+              <h2 className="text-5xl font-bold text-gray-900 sm:text-3xl w-full items-center text-center m-8 uppercase">Best Place Destination</h2>
               <p className="mt-4 max-w-md text-gray-500"></p>
             </header>
 
@@ -306,7 +306,7 @@ const Home = ({ citiesData, stylesData, getInspiredCities }: Props) => {
         <section className="my-24">
           <div className="max-w-2xl mx-auto mt-16 mb-16 text-center">
             <span className="font-bold tracking-wider uppercase dark:text-violet-400"></span>
-            <h2 className="text-4xl font-bold lg:text-5xl">Find your travel style</h2>
+            <h2 className="text-4xl font-bold lg:text-5xl uppercase">Find your travel style</h2>
           </div>
           {/* Map styles data from api */}
           <CarouselTitlesCard travelStyles={stylesData} />
@@ -364,17 +364,17 @@ const Home = ({ citiesData, stylesData, getInspiredCities }: Props) => {
         </section>
 
         <section id="contact" className="py-24 lg:py-36 bg-white overflow-hidden">
-          <div className="container md:p-16 mx-auto">
+          <div className="container mx-auto">
             <div className="flex flex-wrap">
               <div className="w-full md:w-1/2 p-8">
-                <div className="md:max-w-md">
+                <div className=" md:max-w-full">
                   <div className="flex items-center justify-center mb-8 w-20 h-20 rounded-xl" style={{ background: "#000000", backgroundRepeat: "no-repeat", backgroundSize: "100% 100%" }}>
                     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M13.2077 3.19551C13.9184 0.268161 18.0816 0.268162 18.7923 3.19552C19.2514 5.08655 21.4179 5.98396 23.0797 4.97142C25.6522 3.40398 28.596 6.34784 27.0286 8.92031C26.016 10.5821 26.9135 12.7486 28.8045 13.2077C31.7318 13.9184 31.7318 18.0816 28.8045 18.7923C26.9135 19.2514 26.016 21.4179 27.0286 23.0797C28.596 25.6522 25.6522 28.596 23.0797 27.0286C21.4179 26.016 19.2514 26.9135 18.7923 28.8045C18.0816 31.7318 13.9184 31.7318 13.2077 28.8045C12.7486 26.9135 10.5821 26.016 8.92031 27.0286C6.34784 28.596 3.40398 25.6522 4.97142 23.0797C5.98396 21.4179 5.08655 19.2514 3.19552 18.7923C0.268162 18.0816 0.268161 13.9184 3.19551 13.2077C5.08655 12.7486 5.98396 10.5821 4.97142 8.92031C3.40398 6.34784 6.34784 3.40398 8.92031 4.97141C10.5821 5.98396 12.7486 5.08655 13.2077 3.19551Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                       <path d="M21 16C21 18.7614 18.7614 21 16 21C13.2386 21 11 18.7614 11 16C11 13.2386 13.2386 11 16 11C18.7614 11 21 13.2386 21 16Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
                   </div>
-                  <h3 className="mb-6 text-3xl font-semibold" style={{ letterSpacing: "-0.5px" }}>&ldquo;At The Umbrella Expeditions we'll help you plan your trip effectively&amp; productively. Ensuring that you have a seamless holiday even as you travel with us touring all the places of interest !&rdquo;</h3>
+                  <h3 className="mb-6 text-xl md:text-2xl font-semibold" style={{ letterSpacing: "-0.5px" }}>&ldquo;At The Umbrella Expeditions we'll help you plan your trip effectively&amp; productively. Ensuring that you have a seamless holiday even as you travel with us touring all the places of interest !&rdquo;</h3>
                   <h4 className="font-semibold tracking-tight mb-1">Sharon Akinyi</h4>
                   <span>Founder, The Umbrella Expeditions.</span>
                 </div>
